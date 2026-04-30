e71420454ed80559d3acb11baf83689fd8f419bb
Natasha Ringane <natashamathapelo12@gmail.com>
2026-04-30 12:14:06 +0200

    update multidimensional

--- Multidimensional.java
+++ Multidimensional.java
@@ -1,14 +1,16 @@
 public class Multidimensional {
+	public static void main(String[] args) {
 	String[][] info = {
-		{Natasha, Isabela, Tiisetso},
-		{Ayabonga, Lerato, Asanda},
-		{Tumelo, Mahlatse, Karabo}
-	}
+		{"Natasha", "Isabela", "Tiisetso"},
+		{"Ayabonga", "Lerato", "Asanda"},
+		{"Tumelo", "Mahlatse", "Karabo"}
+	};
 	
 	for (int row = 0; row < info.length; row++) {
 		for(int column = 0; column < info[row].length; column++) {
-			System.out.print("Class " + row + ",Student " + column + ": " + info[row][column]);
+			System.out.println("Class " + row + ",Student " + column + ": " + info[row][column]);
 		}
 		System.out.print("");
 	} 
+}
 }
