24916db844dca4074329273b51fec47775f4dffb
Natasha Ringane <natashamathapelo12@gmail.com>
2026-04-30 12:19:29 +0200

    arrays

---
+++ Arrays.java
@@ -0,0 +1,9 @@
+public class Arrays {
+	public static void main(String[] args) {
+		String[] names = {"Natasha", "Isabella", "Tiisetso", "Asanda", "Lerato"};
+		
+		for(int naam = 0; naam < names.length; naam++) {
+			System.out.println("Index " + naam + ": " + names[naam]);
+	}
+}
+}
--- Multidimensional.java
+++ Multidimensional.java
@@ -8,7 +8,7 @@ public class Multidimensional {
 	
 	for (int row = 0; row < info.length; row++) {
 		for(int column = 0; column < info[row].length; column++) {
-			System.out.println("Class " + row + ",Student " + column + ": " + info[row][column]);
+			System.out.println("Class " + row + ", Student " + column + ": " + info[row][column]);
 		}
 		System.out.print("");
 	} 
